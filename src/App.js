4d7e028deaca9e7c0d0fccefdaca7b89299bbe66
Tristan Viney <yenivyeniv@gmail.com>
2019-08-28 16:48:34 +0200

    nlp library added to strip nouns from search input

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React, { Component } from "react"
+
 import Header from "./components/header.js"
 import Search from "./components/search.js"
 import Playlist from "./components/playlist.js"
 import Footer from "./components/footer.js"
 
+import nlp from 'compromise';
+
+// split all CSS into individual files at the end
 import './App.css'
 
 class App extends Component {
@@ -11,11 +15,17 @@ class App extends Component {
     super(props)
     this.handleSearchChange = this.handleSearchChange.bind(this)
     this.handleSearchSubmit = this.handleSearchSubmit.bind(this)
+    this.getNouns = this.getNouns.bind(this)
     this.state = {
       searchInput: "",
+      searchInputNouns: "",
       // playlistValue is only for testing the components are working as expected
       playlistValue: ""
     }
+  }
+
+  getNouns(string) {
+    return nlp(string).nouns().out('array')
   }
 
   handleSearchChange(input) {
@@ -25,6 +35,7 @@ class App extends Component {
   handleSearchSubmit() {
     const searchInput = this.state.searchInput
     this.setState({playlistValue: searchInput})
+    this.setState({searchInputNouns: this.getNouns(this.state.searchInput)})
   }
 
   render() {

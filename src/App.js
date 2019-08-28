4125ae6a72ac05f8e12a01d73e73f3ab8a2fdf7a
Tristan Viney <yenivyeniv@gmail.com>
2019-08-28 12:57:43 +0200

    initial commit beginning layout

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Header extends Component {
+  render() {
+    return (
+      <div className='header'>
+        <h1>{ this.props.title }</h1>
+        <h3>{ this.props.description }</h3>
+      </div>
+    );
+  }
+}
+
+class Search extends Component {
+  render() {
+    return (
+      <div className='search'>
+        <h4>@</h4>
+
+      </div>
+    );
+  }
+}
+
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      search_input: ''
+    }
+  }
+
+  render() {
+    return (
+      <Header
+        title="Twitter Playlist"
+        description="Enter a Twitter handle to create a playlist." />
+    )
+  }
 }
 
 export default App;

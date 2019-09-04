7b25bd718e933d83c10b5892e27a6f3c6ec76802
Tristan Viney <yenivyeniv@gmail.com>
2019-09-04 09:14:58 +0200

    moving on from loader for now

--- src/components/App.js
+++ src/components/App.js
@@ -124,7 +124,6 @@ class App extends Component {
         albumCover: info.album.images[0].url,
         url:        info.external_urls.spotify
       }
-      console.log(songData)
       this.setState({
         songs: this.state.songs.concat(songData)
       })

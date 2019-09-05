321896bc54695d99866258b3136b11b867ae52ee
Tristan Viney <yenivyeniv@gmail.com>
2019-09-05 13:37:18 +0200

    added steaming scope to fix crashes on other peoples machines

--- src/components/spotifyLogin.js
+++ src/components/spotifyLogin.js
@@ -7,7 +7,7 @@ class SpotifyLogin extends Component {
     const clientId      = process.env.REACT_APP_SPOTIFY_CLIENT_ID
     const redirectUri   = "https://plotify-music.herokuapp.com/"
     // const redirectUri   = "http://localhost:3000"
-    const scopes        = ["user-read-email", "user-read-private", "playlist-modify-public"]
+    const scopes        = ["streaming", "user-read-currently-playing", "user-read-private", "playlist-modify-public"]
     const getRequest    = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`
 
     return (

cdbb6e12986126b655372be0756d6ba58ee14b3f
Tristan Viney <yenivyeniv@gmail.com>
2019-09-08 11:58:44 +0200

    added userData load condition to addPlaylist button

--- src/components/App.js
+++ src/components/App.js
@@ -196,7 +196,7 @@ class App extends Component {
                       handleSearchSubmit={this.handleSearchSubmit} />
                   </div>
 
-                  {this.state.songs.length > 0 &&
+                  {(this.state.songs.length > 0 && this.state.userData) &&
                     <SavePlaylist
                       token={this.state.token}
                       userID={this.state.userData.id}
